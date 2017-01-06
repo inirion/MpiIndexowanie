79fef8adfca81da51ac170e5e3c3b42bfac7822a
Grzegorz Kokoszka <grzegorz.r.kokoszka@gmail.com>
2017-01-06 11:55:55 +0100

    Huge Fix
    Rewrited most of code, added print table as class

--- Source.cpp
+++ Source.cpp
@@ -4,98 +4,192 @@
 #include <conio.h>
 #include <iomanip>
 #include <Windows.h>
+#include <vector>
+#include <string>
 
 using namespace std;
-struct timeCount{
+
+struct timeCount {
 	int start;
 	int end;
 	void startTime() { start = GetTickCount(); }
-	void endTime() { end = GetTickCount();}
+	void endTime() { end = GetTickCount(); }
 	int measure() {
 		return end - start;
 	}
 	void reset() { start = 0; end = 0; }
 };
+class StringArray {
+private:
+	int Row;
+	int Column;
+	string Text;
+public:
+	StringArray() : Row(0), Column(0), Text("") {}
+	StringArray(int r, int c, string t) : Row(r), Column(c), Text(t) {}
+	int getRow() { return Row; }
+	int getColumn() { return Column; }
+	string getText() { return Text; }
+	void setText(string text,bool przed = false) {
+		if(!przed)
+			this->Text = text;
+		else {
+			this->Text = text + " " + this->Text;
+		}
+	}
+};
+
+class StringArrayContainer {
+private:
+	int RowCount;
+	int ColCount;
+	vector<StringArray> object;
+public :
+	StringArrayContainer(){}
+	int getRowCount() { return RowCount+1; }
+	int getColCount() { return ColCount+1; }
+	void addElementAt(int row, int col, string text) {
+		object.push_back(StringArray(row, col, text));
+		if (row > RowCount) RowCount = row;
+		if (col > ColCount) ColCount = col;
+	}
+	string getElementAt(int row,int col) {
+		for (int i = 0; i < object.size(); i++) {
+			if (object[i].getRow() == row && object[i].getColumn() == col)
+				return object[i].getText();
+		}
+	}
+	void AddDataFromSource(int *data,int col,int howMuch, int dataSourceSize) {
+		for (int i = dataSourceSize - howMuch, count = 2; i < dataSourceSize; i++, count++) {
+			for (int j = 1; j <= ColCount; j++) {
+				addElementAt(count, j, "");
+			}
+			addElementAt(count, col, to_string(data[i]));
+		}
+	}
+	void setElementAt(int row, int col, string text) {
+		for (int i = 0; i < object.size(); i++) {
+			if (object[i].getRow() == row && object[i].getColumn() == col)
+				object[i].setText(text);
+		}
+	}
+	void SetDataFromSource(int *data, int col, int howMuch, int dataSourceSize) {
+		for (int i = dataSourceSize - howMuch, count = 2; i < dataSourceSize; i++, count++) {
+			setElementAt(count, col, to_string(data[i]));
+		}
+	}
+	void setTime(int row, int col, string text) {
+		for (int i = 0; i < object.size(); i++) {
+			if (object[i].getRow() == row && object[i].getColumn() == col)
+				object[i].setText(text,true);
+		}
+	}
+	void drawTable() {
+		for (int i = 0; i <= RowCount; i++) {
+			for (int j = 0; j <= ColCount; j++) {
+				cout << setw(10) << left << getElementAt(i, j);
+			}
+			cout << endl;
+		}
+	}
+	void InitializateArrayContainer() {
+		addElementAt(0, 0, "");
+		addElementAt(0, 1, "Threads \032");
+		addElementAt(0, 2, "1");
+		addElementAt(0, 3, "2");
+		addElementAt(0, 4, "4");
+		addElementAt(0, 5, "8");
+		addElementAt(0, 6, "16");
+		addElementAt(1, 0, "X[n] \031");
+		addElementAt(1, 1, "Time    \032");
+		addElementAt(1, 2, "ms");
+		addElementAt(1, 3, "ms");
+		addElementAt(1, 4, "ms");
+		addElementAt(1, 5, "ms");
+		addElementAt(1, 6, "ms");
+		RowCount = 1;
+		ColCount = 6;
+	}
+	
+};
+
+void ParallelPrefixSum(int *X, int *S, unsigned long size, unsigned short threadsNumber)
+{
+	int NumberOfThreads, *sChunks, *s = S;
+#pragma omp parallel num_threads(threadsNumber)
+	{
+		int i;
+#pragma omp single
+		{
+			sChunks = new int[threadsNumber + 1];
+			sChunks[0] = 0;
+		}
+		int ThreadID = omp_get_thread_num();
+		int sum = 0;
+#pragma omp for schedule(static) 
+		for (i = 0; i<size; i++) {
+			sum += X[i];
+			s[i] = sum;
+		}
+		sChunks[ThreadID + 1] = sum;
+#pragma omp barrier
+		int offset = 0;
+		for (i = 0; i<(ThreadID + 1); i++) {
+			offset += sChunks[i];
+		}
+#pragma omp for schedule(static)
+		for (i = 0; i<size; i++) {
+			s[i] += offset;
+		}
+	}
+}
+
+void SequencePrefixSum(int *X, int *S, unsigned long size) {
+	int i;
+	S[0] = X[0];
+	for (i = 1; i < size; i++) {
+		S[i] = S[i - 1] + X[i];
+	}
+}
+
+void RandomArrayFill(int *X, int size, int minRange, int maxRange) {
+	int i;
+	for (i = 0; i < size; i++) {
+		X[i] = minRange + (rand() % (int)(maxRange - minRange + 1));
+	}
+}
+
 int main() {
 	srand(time(NULL));
-	int N, *X, *S, *Sp;
+	StringArrayContainer obj;
+	obj.InitializateArrayContainer();
+	int N, *X, *S, *Sparallel, i = 0;
+	int ileOstatnichWyswietlic = 20;
 	cout << "Podaj wielkosc tablicy X[n], S[n]." << endl << "n = ";
 	cin >> N;
 	cout << endl;
 	timeCount timediff{ 0,0 };
-	timeCount timediffP{ 0,0 };
 	X = new int[N];
-	S = new int[N + 1];
-	Sp = new int[N + 1];
-	
-	for (int i = 0; i < N; i++) {
-		X[i] = (rand() % 20) - (rand() % 20);
-	}
-	
-	//Sekwencyjne
+	S = new int[N];
+	Sparallel = new int[N];
+	RandomArrayFill(X, N, -50, 50);
+	obj.AddDataFromSource(X, 0, ileOstatnichWyswietlic, N);
 	timediff.startTime();
-	S[0] = 0;
-	for (int i = 0; i < N; i++) {
-		S[i+1] = S[i] + X[i];
-	}
+	SequencePrefixSum(X,S,N);
+	obj.SetDataFromSource(S, 2, ileOstatnichWyswietlic, N);
 	timediff.endTime();
-	cout << setw(5) << "X[]" << setw(3) << "|" << setw(5) << "S[]" << endl;
-	for (int i = 0; i <= 20; i++) {
-		if (i<N)
-			cout << setw(5) << right << X[i];
-
-		if (i == N) {
-			cout << setw(8) << right << "|";
-			cout << setw(2) << " ";
-			cout << setw(5) << left << S[i];
-		}
-
-		else {
-			cout << setw(3) << "|";
-			cout << setw(2) << " ";
-			cout << setw(5) << left << S[i];
-		}
-
-		cout << endl;
-	}
-	
-	cout << "Uplynelo " << timediff.measure() << "ms - Sekwencyjne" << endl << endl << endl;
-	//Koniec Sekwencyjnego 
-	//Równoleg³e
+	obj.setTime(1, 2, to_string(timediff.measure()));
+	timediff.reset();
 	for (int j = 0; j < 4; j++) {
-		
-		timediffP.startTime();
-		Sp[0] = 0;
-		int i;
+		timediff.startTime();
 		int threadsNumber = 2 << j;
-#pragma omp parallel for private(i) shared(N,X) num_threads(threadsNumber)
-		for (i = 0; i < N; i++) {
-			Sp[i + 1] = Sp[i] + X[i];
-		}
-		timediffP.endTime();
-		cout << setw(5) << right << "X[]" << setw(3) << "|" << setw(5) << "S[]" << endl;
-		for (int i = 0; i <= 20; i++) {
-			if (i < N)
-				cout << setw(5) << right << X[i];
-
-			if (i == N) {
-				cout << setw(8) << right << "|";
-				cout << setw(2) << " ";
-				cout << setw(5) << left << Sp[i];
-			}
-
-			else {
-				cout << setw(3) << "|";
-				cout << setw(2) << " ";
-				cout << setw(5) << left << Sp[i];
-			}
-
-			cout << endl;
-		}
-		cout << "Uplynelo " << timediffP.measure() << "ms - Parallel. Dla " << threadsNumber <<" watkow" << endl << endl << endl;
-		timediffP.reset();
+		ParallelPrefixSum(X, Sparallel, N, threadsNumber);
+		timediff.endTime();
+		obj.SetDataFromSource(S, j+3, ileOstatnichWyswietlic, N);
+		obj.setTime(1, j + 3, to_string(timediff.measure()));
+		timediff.reset();
 	}
-	//Koniec Równoleg³ego
+	obj.drawTable();
 	_getch();
 	return 0;
 }
